35708aa61fdffa41d58b45d7cc4ffee9c4531ebd
De La Torre Lara Arturo <arturo.delato@hotmail.com>
2017-09-11 10:59:07 -0500

    Intercambio inicial de proyecto

---
+++ Cuestionario.java
@@ -0,0 +1,5 @@
+
+public class Cuestionario
+{
+    public void aplicarPregunta(Pregunta p)
+}
---
+++ Pregunta.java
@@ -0,0 +1,27 @@
+
+public class Pregunta
+{
+    private String texto;
+    private String respuesta;
+    
+    public Pregunta()
+    {
+        texto = ' ';
+    }
+    
+    public void asignarTexto(String pre)
+    {
+    }
+    
+    public void asignarRespuesta(String res)
+    {
+    }
+    
+    public boolean checarRespuesta(String resp)
+    {
+    }
+    
+    public imprimete()
+    {
+    }
+}
